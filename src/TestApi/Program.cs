dd702b80e2aa1143c4ab81fa9b10497ae05755ad
Tommaso Stocchi <tommasostocchi@outlook.com>
2021-10-20 19:44:02 +0200

    Added SlowAPI

--- src/TestApi/Program.cs
+++ src/TestApi/Program.cs
@@ -2,5 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/fastapi", () => "Hello World!");
+app.MapGet("/slowapi", () =>
+{
+    Thread.Sleep(1);
+    return "Sorry for the delay... Hello World!";
+});
+
 
 app.Run();
