2dd0adff9139497672dc6bb704de403e3861499d
Tommaso Stocchi <tommasostocchi@outlook.com>
2021-11-29 15:48:28 +0100

    Reset

--- src/TestApi/Program.cs
+++ src/TestApi/Program.cs
@@ -3,10 +3,4 @@ var app = builder.Build();
 
 app.MapGet("/fastapi", () => "Hello World!");
 
-app.MapGet("/slowapi", () =>
-{
-    Thread.Sleep(1);
-    return "Sorry for the delay... Hello World!";
-});
-
 app.Run();
